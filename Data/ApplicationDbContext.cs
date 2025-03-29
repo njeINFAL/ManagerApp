924cbb17c5fc4021ac80204c260f960f07377f68
matekovago <dzmwso@hallgato.uni-neumann.hu>
2025-03-29 13:31:24 +0100

    project structure to the database ceration
    using EF and MVC with autentication

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManagerApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public IConfiguration _config { get; set; }
+        public ApplicationDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
