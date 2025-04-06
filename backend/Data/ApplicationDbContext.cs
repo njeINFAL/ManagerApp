12c58cb6f86d9690c7655d473866e9c2bd3e461f
matekovago <dzmwso@hallgato.uni-neumann.hu>
2025-04-06 09:59:42 +0200

    AddResponsibleUser to the WorkOrderServices table

--- backend/Data/ApplicationDbContext.cs
+++ backend/Data/ApplicationDbContext.cs
@@ -35,15 +35,20 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
             .OnDelete(DeleteBehavior.SetNull);
 
         builder.Entity<WorkOrderService>()
-            .HasOne(wos => wos.WorkOrder)
-            .WithMany(wo => wo.WorkOrderServices)
-            .HasForeignKey(wos => wos.WorkOrderId);
+              .HasOne(wos => wos.WorkOrder)
+              .WithMany(wo => wo.WorkOrderServices)
+              .HasForeignKey(wos => wos.WorkOrderId);
 
         builder.Entity<WorkOrderService>()
-           .HasOne(wos => wos.Service)
-           .WithMany(s => s.WorkOrderServices)
-           .HasForeignKey(wos => wos.ServiceId);
+            .HasOne(wos => wos.Service)
+            .WithMany(s => s.WorkOrderServices)
+            .HasForeignKey(wos => wos.ServiceId);
 
+        builder.Entity<WorkOrderService>()
+            .HasOne(wos => wos.ResponsibleUser)
+            .WithMany()
+            .HasForeignKey(wos => wos.ResponsibleUserId)
+            .OnDelete(DeleteBehavior.SetNull);
     }
 
 
--- backend/Models/WorkOrderService.cs
+++ backend/Models/WorkOrderService.cs
@@ -8,5 +8,9 @@
 
         public int ServiceId { get; set; }
         public Service Service { get; set; }
+
+        public string? ResponsibleUserId { get; set; }
+        public ApplicationUser? ResponsibleUser { get; set; }
+
     }
 }
